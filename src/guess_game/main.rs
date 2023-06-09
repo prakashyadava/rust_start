7d36410fbb0846b1ed6e182d3808dfd912f9911e
Prakash Yadava <prakash.kumar.yadava@gmail.com>
2023-06-09 17:24:42 +0530

    feat: added loop and exception handling

--- src/guess_game/main.rs
+++ src/guess_game/main.rs
@@ -7,18 +7,26 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Guess the number!");
-    println!("Input your guess: ");
+    loop {
+        println!("Input your guess: ");
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Faild to read line");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Faild to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
-    println!("You guessed: {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small!"),
-        Ordering::Equal => println!("You Win!"),
-        Ordering::Greater => println!("Too Big!")
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            },
+            Ordering::Greater => println!("Too Big!")
+        }
     }
 
 }
